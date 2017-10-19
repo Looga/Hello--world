fbfd701e0fcd02d42f101d7d91524f546245f223
Looga <792355215@qq.com>
2017-10-19 21:19:42 +0800

    输出double类型的最值

---
+++ 输出double类型的最值.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<float.h>
+#include<stdlib.h>
+int main (void){
+	double d1=DBL_MIN;
+	double d2=DBL_MAX;
+	printf("%e-----%e",d1,d2);
+	system("pause");
+	return 0;
+	
+} 
