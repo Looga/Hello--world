42c5485956069c7917b8c979910a7f664024f73b
Looga <792355215@qq.com>
2018-01-05 12:07:42 +0800

    DC

---
+++ DC.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	char n;
+	scanf("%c",&n);
+	switch (n){
+		case '1': printf("Wonder Woman");
+		break;
+		case '2': printf("HHH");
+		break;
+		default :printf("Da Ben Dan");
+	}
+	return 0;
+} 
